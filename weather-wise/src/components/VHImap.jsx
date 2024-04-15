28f8bb6478c1d380d60d66bbf42ec527cb5f4769
diogu <diogomiguel.fernandes@ua.pt>
2024-04-15 16:25:55 +0100

    Fixed the map render

--- weather-wise/src/components/VHImap.jsx
+++ weather-wise/src/components/VHImap.jsx
@@ -1,12 +1,13 @@
 import React from 'react';
 import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
+import 'leaflet/dist/leaflet.css';
 
 const VHImap = () => {
     return (
       <div style={{ width: '100%', height: '500px' }}>
         <MapContainer
           center={[39.3999, -8.2245]} // Coordinates for Portugal
-          zoom={6}
+          zoom={10}
           style={{ width: '100%', height: '100%' }}
         >
           <TileLayer
