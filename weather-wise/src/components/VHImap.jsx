69c995af689c4020955c6115e3dc32eea62edc9f
TKahk <pedro.salgado@ua.pt>
2024-04-17 01:04:07 +0100

    Mapa

--- weather-wise/src/components/VHImap.jsx
+++ weather-wise/src/components/VHImap.jsx
@@ -1,18 +1,76 @@
-import React from 'react';
-import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
+import React, { useEffect } from 'react';
+import { MapContainer, TileLayer, Marker, Popup, GeoJSON} from 'react-leaflet';
 import 'leaflet/dist/leaflet.css';
+import "./Map.css";
+import distritos from '../data/Portugal.json';
+import conselhos from '../data/Conselhos.json';
 
 const VHImap = () => {
-    return (
-      <div style={{ width: '100%', height: '500px' }}>
+
+  const getRandomColor = () => {
+    const letters = '0123456789ABCDEF';
+    let color = '#';
+    for (let i = 0; i < 6; i++) {
+      color += letters[Math.floor(Math.random() * 16)];
+    }
+    return color;
+  };
+
+  // Função para mapear o valor de VHI para uma cor
+  const mapVHIToColor = (vhi) => {
+    if (vhi < 15) {
+      return '#ff0000'; // Vermelho
+    } else if (vhi < 30) {
+      return '#ff8000'; // Laranja
+    } else if (vhi < 45) {
+      return '#ffff00'; // Amarelo
+    } else if (vhi < 60) {
+      return '#80ff00'; // Verde claro
+    } else if (vhi < 75) {
+      return '#00ff80'; // Verde
+    } else if (vhi < 90) {
+      return '#00ffff'; // Azul claro
+    } else {
+      return '#0080ff'; // Azul
+    }
+  };
+
+  // Função para definir a cor de preenchimento de cada distrito
+  const styleFunction = (feature) => {
+    const randomColor = getRandomColor(); // Gerar uma cor aleatória para cada distrito
+    const vhiValue = Math.floor(Math.random() * 101); // Simulando um valor aleatório de VHI de 0 a 100
+    const fillColor = mapVHIToColor(vhiValue); // Mapear o valor de VHI para uma cor
+    return {
+      fillColor: fillColor,
+      weight: 1,
+      opacity: 1,
+      color: 'white',
+      fillOpacity: 0.7
+    };
+  };
+
+  const onEachCity = (district, layer) => {
+    if (district.properties && district.properties.Concelho) {
+      layer.on({
+        mouseover: (e) => {
+          layer.bindTooltip(district.properties.Concelho).openTooltip();
+        },
+        mouseout: (e) => {
+          layer.closeTooltip();
+        }
+      });
+    }
+  };
+  
+  return (
+      <div>
         <MapContainer
           center={[39.3999, -8.2245]} // Coordinates for Portugal
-          zoom={10}
-          style={{ width: '100%', height: '100%' }}
+          zoom={7}
+          style={{height: '70vh' }}
+          dragging={true}
         >
-          <TileLayer
-            url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-          />
+        <GeoJSON data={conselhos.features} onEachFeature={onEachCity} style={styleFunction} />
         </MapContainer>
       </div>
     );
--- weather-wise/src/pages/Crops.jsx
+++ weather-wise/src/pages/Crops.jsx
@@ -14,7 +14,24 @@ function Crops() {
         <div className="py-10">
           <h2 className="text-4xl font-bold text-gray-800">Crops</h2>
         </div>
-        <VHImap></VHImap>
+        <div className="flex">
+          <div className="w-1/2" style={{paddingRight: '20px'}}>
+            <div className="border border-gray-300 rounded-md overflow-hidden">
+              <div className="legend">
+                <div className="legend-item" style={{ backgroundColor: '#ff0000', justifyContent:'center' }}>0-14</div>
+                <div className="legend-item" style={{ backgroundColor: '#ff8000' }}>15-29</div>
+                <div className="legend-item" style={{ backgroundColor: '#ffff00' }}>30-44</div>
+                <div className="legend-item" style={{ backgroundColor: '#80ff00' }}>45-59</div>
+                <div className="legend-item" style={{ backgroundColor: '#00ff80' }}>60-74</div>
+                <div className="legend-item" style={{ backgroundColor: '#00ffff' }}>75-89</div>
+                <div className="legend-item" style={{ backgroundColor: '#0080ff' }}>90-100</div>
+              </div>
+            </div>
+          </div>
+          <div className="w-1/2">
+            <VHImap />
+          </div>
+        </div>
       </div>
     </>
   );
