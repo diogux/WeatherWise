a93a2cda28889c6130d0b2dbd6a1a2e8b9ff4f67
TKahk <pedro.salgado@ua.pt>
2024-05-14 22:24:14 +0100

    Storms

--- weather-wise/src/components/StormsMap.jsx
+++ weather-wise/src/components/StormsMap.jsx
@@ -5,6 +5,7 @@ import 'leaflet/dist/leaflet.css';
 import countries from '../data/countries.json';
 import huricane from '../data/huricane.png';
 import huricane2 from '../data/huricane2.png';
+import h3 from '../data/h3.png';
 
 const StormsMap = () => {
     const [searchTerm, setSearchTerm] = useState('');
@@ -58,29 +59,58 @@ const StormsMap = () => {
         [27.949526,22.134803]
     ];
 
+    const img5 = [
+        [74.997832,-148.491847],
+        [45.932050,-46.116972]
+    ];
+
+    const img6 = [
+        [12.012458,92.342081],
+        [-49.514510,187.679133]
+    ];
+
+    const img7 = [
+        [76.386849,43.466443],
+        [26.249083,191.541391]
+    ];
+
     return (
         <div>
             <MapContainer
                 center={[39.3999, -8.2245]}
                 zoom={3}
                 zoomControl={false}
-                dragging={false}
+                dragging={true}
                 scrollWheelZoom={false}
                 doubleClickZoom={false}
-                style={{ height: "80vh", width: "80vw" }}
+                //add a max bounds to prevent the user from scrolling too far
+                maxBounds={[
+                    [90, 180],
+                    [-90, -180]
+                ]}
+                style={{ height: "80vh", width: "70vw" }}
             >
                 {/* Adicione a camada da imagem como um overlay */}
                 <SVGOverlay bounds={imageBounds}>
                     <image href="https://maps.weatherforyou.com/0KT4zzIuojnRWn4BTWTCX_OLEgTCckfgPaBRSRZhx3Kx3dO87LLBvuLfWPsW6a/radar-2m/750x562/39.06,-77.4,7/current.png" x={0} y={0} width="100%" height="100%" style={{opacity :0.7}} />
                 </SVGOverlay>
                 <SVGOverlay bounds={img2}>
-                    <image href={huricane} x={0} y={0} width="100%" height="100%" style={{opacity :0.7}} />
+                    <image href={huricane} x={0} y={0} width="100%" height="100%" style={{opacity :0.5}} />
                 </SVGOverlay>
                 <SVGOverlay bounds={img3}>
-                    <image href={huricane} x={0} y={0} width="100%" height="300" style={{opacity :0.7}} />
+                    <image href={huricane} x={0} y={0} width="100%" height="300" style={{opacity :0.5}} />
                 </SVGOverlay>
                 <SVGOverlay bounds={img4}>
-                    <image href={huricane2} x={0} y={0} width="100%" height="100%" style={{opacity :0.7}} />
+                    <image href={huricane2} x={0} y={0} width="100%" height="100%" style={{opacity :0.5}} />
+                </SVGOverlay>
+                <SVGOverlay bounds={img5}>
+                    <image href={h3} x={0} y={0} width="100%" height="100%" style={{opacity :0.5}} />
+                </SVGOverlay>
+                <SVGOverlay bounds={img6}>
+                    <image href={h3} x={0} y={0} width="100%" height="100%" style={{opacity :0.5}} />
+                </SVGOverlay>
+                <SVGOverlay bounds={img7}>
+                    <image href={huricane2} x={0} y={0} width="100%" height="100%" style={{opacity :0.5}} />
                 </SVGOverlay>
                 <GeoJSON
                     data={countries}
--- weather-wise/src/pages/Storms.jsx
+++ weather-wise/src/pages/Storms.jsx
@@ -1,24 +1,73 @@
 import "../App.css";
-import { Button } from "@/components/ui/button";
+import { FaInfoCircle } from 'react-icons/fa';
 import Navbar from "../components/navbar";
-import Footer from "../components/footer";
-import Search from "../components/search";
 import StormsMap from "../components/StormsMap";
 
 function Storms() {
+  const InfoCard = () => (
+    <div className="info-card bg-white/70 p-4 rounded-lg">
+      <div className="flex flex-col items-center">
+        <div className="mb-4">
+          <FaInfoCircle size={24} color="black" />
+        </div>
+        <p className="text-gray-900 text-center">
+          Our Storm Severity Index (SSI) gauges the intensity of storms and hurricanes, guiding preparedness and response. Ranging from minimal impact to extreme danger, it aids in proactive measures and disaster management. The map delineates vulnerable regions for better risk mitigation.
+        </p>
+      </div>
+    </div>
+  );
+
+  const CriticalRegionsCard = () => (
+    <div className="info-card bg-red-500/70 p-4 rounded-lg">
+      <h3 className="text-white text-center font-bold mb-2 text-xl">Critical Regions</h3>
+      <table className="text-white mx-auto">
+        <thead>
+          <tr>
+            <th className="px-2">Region</th>
+            <th className="px-2 pl-10">Coordinates</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td className="px-2 text-center">Moscow</td>
+            <td className="px-2 text-center pl-10">55.7558° N, 37.6173° E</td>
+          </tr>
+          <tr>
+            <td className="px-2 text-center">Arizona</td>
+            <td className="px-2 text-center pl-10">34.0489° N, 111.0937° W</td>
+          </tr>
+          <tr>
+            <td className="px-2 text-center">Lisbon</td>
+            <td className="px-2 text-center pl-10">39.3999° N, 8.2245° W</td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
+  );
+  
+  
+
   return (
     <>
       <div className="flex flex-col min-h-screen">
-        <Navbar></Navbar>
-        <div className="mx-auto w-full flex-grow pt-10 pl-10 ">
-          <h2 className="text-4xl font-bold text-grey">Storms</h2>
-          <div className="flex flex-col items-center justify-center">
-            <StormsMap />
+        <Navbar />
+        <div className="mx-auto w-full flex-grow pt-10 pl-10 relative">
+          <h2 className="text-4xl font-bold text-grey mb-6">Storms and Hurricanes</h2>
+          <div className="flex w-full mx-auto relative">
+            <div className="w-full md:w-1/4 mb-4 md:mb-0 pr-10">
+              <div className="mb-4">
+                <CriticalRegionsCard />
+              </div>
+              <div className="mb-4">
+                <InfoCard />
+              </div>
+            </div>
+            <div className="w-full md:w-3/4">
+              <StormsMap />
+            </div>
           </div>
         </div>
       </div>
-
-      
     </>
   );
 }
